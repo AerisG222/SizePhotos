8164633aa13fcfec814ea84db29b8e8770854e07
Mike Morano <mmorano@mikeandwan.us>
2016-05-17 20:55:47 -0400

    never auto brighten - this simplifies the code and by deferring optimization to a later stage yields improved results in image quality

--- src/SizePhotos/Raw/RawConverter.cs
+++ src/SizePhotos/Raw/RawConverter.cs
@@ -11,7 +11,6 @@ namespace SizePhotos.Raw
     public class RawConverter
         : IRawConverter
     {
-        const double DARK_THRESHOLD = 250;
         readonly bool _quiet;
         
         
@@ -30,88 +29,18 @@ namespace SizePhotos.Raw
         public async Task<IRawConversionResult> ConvertAsync(string sourceFile)
         {
             var result = new RawConversionResult();
-            
-            result.Mode = DetermineConversionMode(sourceFile);
-            
-            if(!_quiet && result.Mode == RawConversionMode.NonBrightening)
-            {
-                Console.WriteLine($"{sourceFile}: raw conversion using non-brightening mode");
-            }
-            
-            var dcraw = new DCRaw(GetOptimalOptions(result.Mode));
+            var opts = new DCRawOptions {
+                UseCameraWhiteBalance = true,
+                Quality = InterpolationQuality.Quality3,
+                HighlightMode = HighlightMode.Blend,
+                Colorspace = Colorspace.sRGB,
+                DontAutomaticallyBrighten = true
+            };
+            var dcraw = new DCRaw(opts);
             
             result.OutputFile = (await dcraw.ConvertAsync(sourceFile)).OutputFilename;
             
             return result;
-        }
-        
-        
-        RawConversionMode DetermineConversionMode(string photoPath)
-        {
-            if(GetDarkThresholdForRawImage(photoPath) < DARK_THRESHOLD)
-            {
-                return RawConversionMode.NonBrightening;
-            }
-            
-            return RawConversionMode.Brightening;
-        }
-        
-        
-        static double GetDarkThresholdForRawImage(string path)
-        {
-            var opts = new DCRawOptions {
-                HalfSizeColorImage = true,  // try to speed this up, don't need quality here
-                UseCameraWhiteBalance = true,
-                DontAutomaticallyBrighten = true
-            };
-            
-            var dcraw = new DCRaw(opts);
-            var res = dcraw.Convert(path);
-            
-            using(var wand = new MagickWand(res.OutputFilename))
-            {
-                double mean, stddev;
-                
-                // read the image, posterize, then figure out how 'dark' it is
-                wand.PosterizeImage(3, false);
-                wand.GetImageChannelMean(ChannelType.AllChannels, out mean, out stddev);
-                
-                File.Delete(res.OutputFilename);
-
-                return mean;
-            }
-        }
-        
-        
-        static DCRawOptions GetOptimalOptions(RawConversionMode mode)
-        {
-            if(mode == RawConversionMode.NonBrightening)
-            {
-                return GetOptimalNonBrighteningOptions();
-            }
-            
-            return GetOptimalBrighteningOptions();
-        }
-        
-        
-        static DCRawOptions GetOptimalBrighteningOptions()
-        {
-            return new DCRawOptions {
-                UseCameraWhiteBalance = true,
-                Quality = InterpolationQuality.Quality3,
-                HighlightMode = HighlightMode.Blend,
-                Colorspace = Colorspace.sRGB
-            };
-        }
-        
-        
-        static DCRawOptions GetOptimalNonBrighteningOptions()
-        {
-            var opts = GetOptimalBrighteningOptions();
-            
-            opts.DontAutomaticallyBrighten = true;
-            
-            return opts;
         }
     }
 }
