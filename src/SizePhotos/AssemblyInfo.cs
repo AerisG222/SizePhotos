01a83b627b16c67f2e3d8f2a9587bd5cae1b7917
Mike Morano <mmorano@mikeandwan.us>
2017-05-21 15:05:25 -0400

    update to netcoreapp 2.0

--- src/SizePhotos/AssemblyInfo.cs
+++
@@ -1,6 +0,0 @@
-using System.Reflection;
-
-[assembly: AssemblyTitle("SizePhotos")]
-[assembly: AssemblyCompany("Mike Morano")]
-[assembly: AssemblyCopyright("(c) 2016 Mike Morano")]
-[assembly: AssemblyDescription("Application to convert and scale photos in preparation for the web")]
