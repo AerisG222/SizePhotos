579ab140c0c33966df7df759d42e027a67086ab4
Mike Morano <mmorano@mikeandwan.us>
2023-10-26 19:00:22 -0400

    update default processing profile from latest version of app that doesn't wash out colors in certain conditions

--- src/SizePhotos/Converters/RawTherapeeConverter.cs
+++ src/SizePhotos/Converters/RawTherapeeConverter.cs
@@ -19,16 +19,11 @@ public class RawTherapeeConverter
 
         if (IsRawFile(sourceFile))
         {
-            // default to a pre-specified profile (copy of "/usr/share/rawtherapee/profiles/Generic/Natural 1.pp3")
-            // opts.AddUserSpecifiedPp3Source(Path.Combine(AppContext.BaseDirectory, "natural.pp3"));
-            opts.AddUserSpecifiedPp3Source(Path.Combine(AppContext.BaseDirectory, "auto_matched_curve_iso_low.pp3"));
-
-            // now bump the contrast and saturation a bit
+            opts.AddUserSpecifiedPp3Source(Path.Combine(AppContext.BaseDirectory, "standard_film_curve_iso_low.pp3"));
             opts.AddUserSpecifiedPp3Source(Path.Combine(AppContext.BaseDirectory, "contrast_saturation.pp3"));
         }
         else
         {
-            // default to a neutral profile (generated in app)
             opts.AddUserSpecifiedPp3Source(Path.Combine(AppContext.BaseDirectory, "neutral.pp3"));
         }
 
